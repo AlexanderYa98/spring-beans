d7b46ae65d59e8240c16768d0f6dee08e8ce9bb1
AlexanderYa98 <ayakovlev524@gmail.com>
2021-09-24 00:49:40 +0700

    2.1.3

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -2,6 +2,8 @@ package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,13 +12,17 @@ public class AnimalsCage {
     @Autowired
     @Qualifier("dog")
     private Animal animal;
-    Timer timer = new Timer();
+    ApplicationContext applicationContext =
+            new AnnotationConfigApplicationContext(Timer.class);
+    Timer bean =
+            (Timer) applicationContext.getBean("timer");
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(timer.getTime());
+        System.out.println(bean.getTime());
         System.out.println("________________________");
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,4 @@
 import app.config.AppConfig;
-import app.model.AnimalsCage;
 import app.model.Timer;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,16 +17,15 @@ public class MainTest {
 
     @Test
     public void mainTest() {
-        Timer timer = new Timer();
         long time = 0;
         for (int i = 0; i < 5; i++) {
-            AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
+            Timer bean =
+                    (Timer) applicationContext.getBean("timer");
             if (i == 0) {
-                time = timer.getTime();
+                time = bean.getTime();
                 continue;
             }
-            Assert.assertEquals("Тест провален, не корректная реализация бинов.", time, timer.getTime().longValue());
+            Assert.assertEquals("Тест провален, не корректная реализация бинов.", time, bean.getTime().longValue());
             System.out.println(time);
         }
     }
